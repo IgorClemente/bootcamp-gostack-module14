bea2cb577d78494e675452a4498e91b4a9b8c5b0
Igor Clemente <igorelectrofest2011@hotmail.com>
2020-01-12 15:28:30 -0300

    BOOTCAMP_GOSTACK_MODULE14_IOSAPPLICATION-XCODECONFIG_CREATED_12012020

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,8 +1,14 @@
-import LinearGradient from 'react-native-linear-gradient';
+//import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
-export default styled(LinearGradient).attrs({
+export default styled.View`
+  background-color: #fff;
+`;
+
+/** 
+styled(LinearGradient).attrs({
   colors: ['#7159c1', '#ab59c1'],
 })`
   flex: 1;
 `;
+**/
